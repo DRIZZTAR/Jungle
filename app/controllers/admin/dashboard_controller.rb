4430c5cb271b43b307fa8bac2fd2d77084955b7c
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-02-07 12:49:39 -0700

    Use Category.all in controller to access all of categories in admin show to dynamically add or remove admin button categories if new categories are added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV.fetch("ADMIN_USERNAME", "Jungle"), password: ENV.fetch("ADMIN_PASSWORD", "book")
 
   def show
-    
+    @product_count = Product.count
+    @categories = Category.all
   end
 end

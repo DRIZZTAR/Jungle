71a7940bab8a9b885b57f1efdc8f5a893fc23950
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-02-09 12:37:33 -0700

    Install bcrypt, add has_secure_password to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to root_url # Consider using root_url instead of '/'
+    else
+      redirect_to signup_url # Consider using signup_url helper if available
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+

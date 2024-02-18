9d6afcee24d2182c1a501ec008d6169d307e455a
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-02-18 13:19:04 -0700

    add spec to check for more than 3 products on the page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,17 @@
 describe('example to-do app', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:3000/')
-  })
+  });
 
   it('displays the intro greeting', () => {
     cy.get('section.title h1').should('have.text', 'The Jungle')
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are more than 2 products on the page", () => {
+    cy.get(".products article").should("have.length.greaterThan", 2)
   })
-})
+});
